ac48d89704819863ec562a71a96daca0b60b562d
Kerim <kerim.tasci@yahoo.de>
2023-12-21 11:14:45 +0100

    cardaction & select active player

--- ringoffire/src/app/app.module.ts
+++ ringoffire/src/app/app.module.ts
@@ -15,6 +15,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 
@@ -39,6 +41,7 @@ import { GameInfoComponent } from './game-info/game-info.component';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
