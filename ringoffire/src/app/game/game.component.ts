987b1b80dcee0298a4076e2c337846e6062b8232
Kerim <kerim.tasci@yahoo.de>
2023-12-22 10:12:13 +0100

    unabled picking card befor adding min 2 players

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../models/game';
-import { MatDialog,} from '@angular/material/dialog';
+import { MatDialog, } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
@@ -10,55 +10,64 @@ import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  game : Game;
-  currentCard : string = '';
+  game: Game;
+  currentCard: string = '';
 
-  constructor(public dialog: MatDialog) {};
+  constructor(public dialog: MatDialog) { };
 
 
   ngOnInit(): void {
     this.newGame();
   }
 
-  newGame(){
+  newGame() {
     this.game = new Game();
     console.log(this.game)
   }
 
-  takeCard(){
+  takeCard() {
+
+
     if (!this.pickCardAnimation) {
-      
-    this.currentCard = this.game.stack.pop();
-    this.pickCardAnimation = true;
+      if (this.game.players.length > 1) {
 
-    this.game.currentPlayer++;
-    this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+        this.currentCard = this.game.stack.pop();
+        this.pickCardAnimation = true;
 
-    setTimeout(() => {
-      this.game.playedCards.push(this.currentCard)
-      this.pickCardAnimation = false;
-    }, 1250);
-  }
+        this.game.currentPlayer++;
+        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
 
-}
-
-openDialog(): void {
-  if (this.game.players.length < 6) {
-    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
-    
-    });
-  
-    dialogRef.afterClosed().subscribe((name: string) => {
-      if (name && name.length > 0) {
-        this.game.players.push(name)
+        setTimeout(() => {
+          this.game.playedCards.push(this.currentCard)
+          this.pickCardAnimation = false;
+        }, 1250);
       }
       
-    });
-  } else {
-    alert('Maximum 6 players')
+      else {
+        alert('Please add 2-6 players before starting the game')
+      }
+
+    }
+
   }
-  
-}
+
+  openDialog(): void {
+    if (this.game.players.length < 6) {
+      const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
+
+      });
+
+      dialogRef.afterClosed().subscribe((name: string) => {
+        if (name && name.length > 0) {
+          this.game.players.push(name)
+        }
+
+      });
+    } else {
+      alert('Maximum 6 players')
+    }
+
+  }
 }
 
 
